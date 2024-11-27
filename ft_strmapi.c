04478b406e25b5e39cbaedb0b4f024303034b75b
Lvvz <93782676+luuzthebest@users.noreply.github.com>
2024-11-27 14:38:26 +0100

    forbidden function used by mistake
    strlen() instead of ft_strlen()

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: hounajar <hounajar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 17:14:56 by hounajar          #+#    #+#             */
-/*   Updated: 2024/11/20 18:56:58 by hounajar         ###   ########.fr       */
+/*   Updated: 2024/11/27 14:34:45 by hounajar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	i = 0;
 	if (!s || !f)
 		return (ft_strdup(""));
-	mapped = malloc(strlen(s)+1);
+	mapped = malloc(ft_strlen(s)+1);
 	if (mapped == NULL)
 		return (NULL);
 	while (s[i])
